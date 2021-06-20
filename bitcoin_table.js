700a3d79052cdb25f286d284d60fe03a36874c21
Q-100 <opea5954@gmail.com>
2021-06-21 02:56:07 +0900

    erningrate module

--- bitcoin_table.js
+++ bitcoin_table.js
@@ -1,10 +1,12 @@
-function gap_calc(coin_json, data) {
+function gap_calc(coin_json, data) { // 업비트 빗썸 가격 계산
     let ask_num = 14;
     let bid_num = 15;
     let bithumb = JSON.parse(data);
     let buySellGb_tmp = bithumb.content.list[0].buySellGb;
-    let contPrice_tmp = bithumb.content.list[0].contPrice;
+    let contPrice_tmp = bithumb.content.list[0].contPrice; // 현재가
     let gap_price = 0;
+
+
 
     console.log("현재가" + contPrice_tmp);
     if (buySellGb_tmp == 1) {
@@ -31,9 +33,12 @@ function gap_calc(coin_json, data) {
             print_checkbox(14); // ask 호가창 
         }
     }
-    //console.log("빗썸 현재가 : " + contPrice_tmp, "차이 : " + gap_price, "gb : " + buySellGb_tmp);
+    //console.log("현재가 : " + contPrice_tmp, "차이 : " + gap_price, "gb : " + buySellGb_tmp);
+    earning_rate_calc(contPrice_tmp);
 
-    function print_table(sign, gap) {
+
+
+    function print_table(sign, gap) { // 호가창 출력
         for (let i = 0; i < 15; i++) {
             let ask_price_tmp = document.getElementById("t" + ask_num + "_price");
             let ask_size_tmp = document.getElementById("t" + ask_num + "_amount");
@@ -60,7 +65,7 @@ function gap_calc(coin_json, data) {
             bid_num += 1;
         }
     }
-    function print_checkbox(num) {
+    function print_checkbox(num) { // 호가창 체결 박스
         let cur_box_ask = document.getElementById("t" + num);
         if (num == 15) {
             let reset_box = document.getElementById("t14");
@@ -72,4 +77,13 @@ function gap_calc(coin_json, data) {
         }
         cur_box_ask.style.border = "solid";
     }
+    function earning_rate_calc(contPrice_tmp) { // 수익률 계산
+        let earning_rate_id = document.getElementById("earning_rate");
+        let my_average_price = localStorage.getItem("my_average_price");
+        let earning_rate = ((parseInt(contPrice_tmp) - parseInt(my_average_price)) / parseInt(my_average_price)) * 100;
+        console.log(earning_rate);
+        earning_rate = Math.round(earning_rate * 100) / 100;
+        earning_rate_id.innerText = earning_rate + "%";
+    }
 }
+
--- order.js
+++ order.js
@@ -1,19 +1,35 @@
-localStorage.setItem("KRW", 100000);
-let KRW = localStorage.getItem("KRW");
-let BTC = localStorage.getItem("BTC");
-console.log("현재 보유 KRW : ", KRW);
+//localStorage.setItem("KRW", 100000);
+//localStorage.setItem("BTC", 0);
+//localStorage.setItem("ask_bid", 0);
+//localStorage.setItem("ask_price", 0);
+let KRW_cur = localStorage.getItem("KRW");
+let BTC_cur = localStorage.getItem("BTC");
+console.log("현재 보유 KRW : ", KRW_cur);
+
 
 function order_coin(all_price, buy_price) {
-    console.log(KRW, BTC);
-    console.log("주문 총액 : ", all_price, "주문가격:", buy_price);
-    if (KRW < all_price) {
+    let cur_price = document.getElementById("t14_price").innerText; // 현재가
+    if (parseInt(KRW_cur) < parseInt(all_price)) {
         alert("잔액이 부족합니다.");
     }
     else {
-        let BTC_tmp = all_price / buy_price;
-        BTC_tmp.toFixed(5);
-        localStorage.setItem("BTC", BTC + BTC_tmp);
-        localStorage.setItem("KRW", KRW - all_price);
-        alert(BTC_tmp + "개의 BTC를 구입하였습니다.")
+        localStorage.setItem("ask_price", buy_price);
+        if (parseInt(buy_price) >= parseInt(cur_price)) {
+            let BTC_tmp = parseInt(all_price) / parseInt(cur_price); // 현재가보다 높은 가격을 호가하면 현재가에 사짐
+            BTC_tmp = parseFloat(BTC_tmp) + parseFloat(localStorage.getItem("BTC"));
+            BTC_tmp.toFixed(5);// 비트코인 자릿수 수정할 것
+            let KRW_tmp = parseInt(localStorage.getItem("KRW")) - parseInt(all_price);
+            localStorage.setItem("BTC", BTC_tmp);
+            localStorage.setItem("KRW", KRW_tmp);
+            alert(parseInt(all_price) / parseInt(buy_price) + "개의 BTC를 구입하였습니다.(개당" + cur_price + "원)");
+            localStorage.setItem("ask_price", -1); // 구매되었으니 매수주문 삭제
+            localStorage.setItem("my_average_price", cur_price); // 구매 가격
+            let my_coin = document.getElementById("my_btc");
+            my_coin.innerText = localStorage.getItem("BTC") + "BTC"; // 현재 코인갯수
+
+        }
+        else {
+            alert();
+        }
     }
 }
